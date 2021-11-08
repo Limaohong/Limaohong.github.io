e75e06d5f75589bb40a73efdf2c81eb4c162489d
Limaohong <n531396008@gmail.com>
2021-11-08 15:25:14 +0800

    add inputSearch event and other html css adjust

--- js/hw5.js
+++ js/hw5.js
@@ -32,59 +32,97 @@ let travelData = [
 ];
 
 let nameArr = ['ticketName', 'ticketImgUrl', 'ticketRegion', 'ticketPrice', 'ticketNum', 'ticketRate', 'ticketDescription']
+let tempData = [];
+let tempState = [
+    {
+        'name':'inputText',
+        'state':0
+    },
+    {
+        'name':'regionSelect',
+        'state':0
+    },
+];
 
 //初始化
 window.onload = function () {
-    test();
+    messageHide();
     addTravelCard();
-    addCard();
-    selectChange();
+    evtControl();
 }
 
+/**
+ * 事件控制
+ */
+function evtControl(){
+    btnAddCard();
+    regionSelect();
+    inputTextSelect();
+}
+
+/**
+ * 畫面初始card render/一般執行card render
+ * @param {*} inputDataArr 
+ */
 function addTravelCard(inputDataArr) {
     let ulElement = document.querySelector('ul');
     let newDataContnet = '';
     let excuateArr = [];
-    inputDataArr ? excuateArr = inputDataArr : excuateArr = travelData;
+    if(inputDataArr) {
+        excuateArr = inputDataArr;
+    } else {
+        excuateArr = travelData;
+        tempData = travelData;
+    }
     while (ulElement.hasChildNodes()) {
         ulElement.removeChild(ulElement.firstChild);
     }
-    excuateArr.forEach(data => {
-        newDataContnet +=
-            `
-        <li class="ticketCard">
-            <div class="ticketCard-img">
-                <a href="#">
-                <img src="${data.imgUrl}" alt="">
-                </a>
-                <div class="ticketCard-region">${data.area}</div>
-                <div class="ticketCard-rank">${data.rate}</div>
-            </div>
-            <div class="ticketCard-content">
-                <div>
-                <h3>
-                    <a href="#" class="ticketCard-name">${data.name}</a>
-                </h3>
-                <p class="ticketCard-description">
-                    ${data.description}
-                </p>
+    if(excuateArr.length == 0) {
+        document.querySelector('.cantFind-area').style.display = 'block';
+    } else {
+        document.querySelector('.cantFind-area').style.display = 'none';
+
+        excuateArr.forEach(data => {
+            newDataContnet +=
+                `
+            <li class="ticketCard">
+                <div class="ticketCard-img">
+                    <a href="#">
+                    <img src="${data.imgUrl}" alt="">
+                    </a>
+                    <div class="ticketCard-region">${data.area}</div>
+                    <div class="ticketCard-rank">${data.rate}</div>
                 </div>
-                <div class="ticketCard-info">
-                <p class="ticketCard-num">
-                    <span><i class="fas fa-exclamation-circle"></i></span>
-                    剩下最後 <span id="ticketCard-num"> ${data.group} </span> 組
-                </p>
-                <p class="ticketCard-price">
-                    TWD <span id="ticketCard-price">$${data.price}</span>
-                </p>
+                <div class="ticketCard-content">
+                    <div>
+                    <h3>
+                        <a href="#" class="ticketCard-name">${data.name}</a>
+                    </h3>
+                    <p class="ticketCard-description">
+                        ${data.description}
+                    </p>
+                    </div>
+                    <div class="ticketCard-info">
+                    <p class="ticketCard-num">
+                        <span><i class="fas fa-exclamation-circle"></i></span>
+                        剩下最後 <span id="ticketCard-num"> ${data.group} </span> 組
+                    </p>
+                    <p class="ticketCard-price">
+                        TWD <span id="ticketCard-price">$${data.price}</span>
+                    </p>
+                    </div>
                 </div>
-            </div>
-        </li>`;
-    })
-    ulElement.innerHTML += newDataContnet;
+            </li>`;
+        })
+        ulElement.innerHTML += newDataContnet;
+    }
+    
 }
 
-function addCard() {
+/**
+ * button click
+ */
+function btnAddCard() {
     let addBtn = document.getElementById('addCard');
     let ulElement = document.querySelector('ul');
     addBtn.addEventListener('click', (e) => {
@@ -96,9 +134,9 @@ function addCard() {
         let ticketRate = document.querySelector('#ticketRate').value;
         let ticketDescription = document.querySelector('#ticketDescription').value;
 
-        if(inputVerify()){
+        if (inputVerify()) {
             let newDataContnet =
-            `
+                `
             <li class="ticketCard">
                 <div class="ticketCard-img">
                     <a href="#">
@@ -127,57 +165,135 @@ function addCard() {
                     </div>
                 </div>
             </li>`;
-        ulElement.innerHTML += newDataContnet;
+            ulElement.innerHTML += newDataContnet;
 
-        let travelDataLen = travelData.length;
-        let travelDataObj = {
-            "id": travelDataLen,
-            "name": ticketName,
-            "imgUrl": ticketImgUrl,
-            "area": ticketRegion,
-            "description": ticketDescription,
-            "group": ticketName,
-            "price": ticketPrice,
-            "rate": ticketRate,
-        }
-        travelData.push(travelDataObj);
+            let travelDataLen = travelData.length;
+            let travelDataObj = {
+                "id": travelDataLen,
+                "name": ticketName,
+                "imgUrl": ticketImgUrl,
+                "area": ticketRegion,
+                "description": ticketDescription,
+                "group": ticketName,
+                "price": ticketPrice,
+                "rate": ticketRate,
+            }
+            travelData.push(travelDataObj);
         }
 
     })
 }
 
-function inputVerify(){
+/**
+ * 必填(錯誤訊息)提示
+ */
+function inputVerify() {
     let flag = true;
     nameArr.forEach(item => {
-        let itemVal = document.querySelector('#'+item).value;
-        if(!itemVal) {
+        let itemVal = document.querySelector('#' + item).value;
+        if (!itemVal) {
             flag = false;
-            document.querySelector('#i_'+item).style.display = 'block';
+            document.querySelector('#i_' + item).style.display = 'block';
         }
     })
     return flag;
 }
+/**
+ * 執行card render function && 改變搜尋筆數
+ * @param {*} cardArray 
+ */
+function renderCard(cardArray) {
+    if(cardArray.length >= 1) {
+        tempData = cardArray;
+    }
+    let searchResultText = document.getElementById('searchResult-text');
+    searchResultText.textContent = `本次搜尋共 ${cardArray.length} 筆資料`;
+    addTravelCard(cardArray);
+}
 
-function selectChange() {
+/**
+ * 地區選擇事件
+ */
+function regionSelect() {
     let regionSearch = document.querySelector('.regionSearch');
     regionSearch.addEventListener('change', (e) => {
-        let selectDataArr = [];
-        travelData.forEach(data => {
-            let area = e.target.value;
-            if (area == 'ALL') {
-                selectDataArr = travelData;
-            } else if (data['area'] == area) {
-                selectDataArr.push(data);
+        let area = e.target.value;
+        let state = 0;
+        tempState.forEach(data => {
+            if(area != 'ALL') {
+                if(data['name'] == 'regionSelect'){
+                    data['state'] = 1;
+                }
+            } else {
+                if(data['name'] == 'regionSelect'){
+                    data['state'] = 0;
+                }
             }
+            state += data['state'];
         })
-        let searchResultText = document.getElementById('searchResult-text');
-        searchResultText.textContent = `本次搜尋共 ${selectDataArr.length} 筆資料`;
-        addTravelCard(selectDataArr);
+        if(state <= 1) {
+            tempData = travelData
+        }
+        let rtnArr = tempData.filter(data => {
+            return area =='ALL'?tempData:data['area'] == area
+        })
+        if(area == 'ALL') {
+            let textSearch = document.getElementById('searchInput');
+            triggerEvt(textSearch,'keyup');
+            rtnArr = tempData;
+        }
+        renderCard(rtnArr);
+    })
+}
+/**
+ * input打字事件
+ */
+function inputTextSelect() {
+    let textSearch = document.getElementById('searchInput');
+    textSearch.addEventListener('keyup', (e) => {
+        let inputText = e.target.value;
+        let state = 0;
+        tempState.forEach(data => {
+            if(inputText != '') {
+                if(data['name'] == 'inputText'){
+                    data['state'] = 1;
+                }
+            } else {
+                if(data['name'] == 'inputText'){
+                    data['state'] = 0;
+                }
+            }
+            state += data['state'];
+        })
+        if(state <= 1) {
+            tempData = travelData
+        }
+        let rtnArr = tempData.filter(data => {
+            return data.name.includes(inputText);
+        })
+        if(inputText == '') {
+            let regionSearch = document.querySelector('.regionSearch');
+            triggerEvt(regionSearch,'change');
+            rtnArr = tempData;
+        }
+        renderCard(rtnArr);
     })
 }
 
-function test() {
+/**
+ * 畫面初始化影藏alert message
+ */
+function messageHide() {
     nameArr.forEach(item => {
-        document.querySelector('#i_'+item).style.display = 'none';
+        document.querySelector('#i_' + item).style.display = 'none';
     })
+}
+
+/**
+ * 觸發change事件
+ * @param {*} el 
+ */
+function triggerEvt(el,event){
+    let evt = new Event(event,{})
+    el.dispatchEvent(evt);
 }
